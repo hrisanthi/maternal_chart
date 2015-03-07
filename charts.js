51764816e25c3a56372b0640fbd48e51393c7d31
hrisanthi <hrisanthi.kroi@gmail.com>
2015-03-07 09:50:16 -0500

    chart js edits

--- charts.js
+++ charts.js
@@ -1,25 +1,95 @@
-$(function () { 
+$(document).ready(function(){    
+    loadData();
+});
+
+
+function loadData() {
+    
+    $.ajax({
+        type: "GET",
+        url: "data.json",
+        dataType: "text",
+        success: parseData
+    });
+    //code
+};
+function parseData(data) {
+    
+    screenWidth = $(window).width();
+    if (screenWidth <= 410) {
+        isPhone = true;
+        //code      
+    }
+    
+    writeChart();
+    //code
+};
+function writeChart() {
     $('#container').highcharts({
         chart: {
             type: 'bar'
         },
         title: {
-            text: 'Fruit Consumption'
+            text: 'Obsecales'
         },
-        xAxis: {
-            categories: ['Apples', 'Bananas', 'Oranges']
+        subtitle: {
+            text: 'Information from DHS'
         },
-        yAxis: {
+        xAxis: [{
+            categories: educaiton
+        },{
+            categories: wealth
+        },{
+            categories: residence
+        },
+            
             title: {
-                text: 'Fruit eaten'
+                text: null
+            }]
+
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Percent',
+                align: 'high'
+            },
+            labels: {
+                overflow: 'justify'
             }
         },
+        tooltip: {
+            valueSuffix: ' percent'
+        },
+        plotOptions: {
+            bar: {
+                dataLabels: {
+                    enabled: true
+                }
+            }
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'top',
+            x: 0,
+            y: 100,
+            floating: true,
+            borderWidth: 1,
+            backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
+            shadow: false
+        },
+        credits: {
+            enabled: false
+        },
         series: [{
-            name: 'Jane',
-            data: [1, 0, 4]
+            name: 'none',
+            data: none
         }, {
-            name: 'John',
-            data: [5, 7, 3]
+            name: 'primary',
+            data: primary
+        }, {
+            name: 'secondary',
+            data: secondary
         }]
-    });
+    };
 });
---
+++ graphtheme.js
@@ -0,0 +1,35 @@
+Highcharts.theme = {
+    colors: ['#a2aa5d', '#8c934f', '#e4782e', '#c7682e', '#b7b7b7', '#fff', 
+             '#231d1f'],
+    chart: {
+        backgroundColor: {
+            linearGradient: [0, 0, 500, 500],
+            stops: [
+                [0, 'rgb(255, 255, 255)'],
+                [1, 'rgb(240, 240, 255)']
+            ]
+        },
+    },
+    title: {
+        style: {
+            color: '#000',
+            font: 'bold 16px "Trebuchet MS", Verdana, sans-serif'
+        }
+    },
+    subtitle: {
+        style: {
+            color: '#000',
+            font: 'bold 12px "Trebuchet MS", Verdana, sans-serif'
+        }
+    },
+
+    legend: {
+        itemStyle: {
+            font: '9pt Trebuchet MS, Verdana, sans-serif',
+            color: 'black'
+        },
+        itemHoverStyle:{
+            color: 'gray'
+        }   
+    }
+};
